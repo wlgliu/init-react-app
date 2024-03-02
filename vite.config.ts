a4239df6f4cc70f46c5215c3171debc785590b12
wangleigang <wangleigang@funji.club>
2024-03-02 10:42:08 +0800

    pwa使用最少配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,39 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
+import mkcert from 'vite-plugin-mkcert'
+
+const pwaOptions: Partial<VitePWAOptions> = {
+  includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+  manifest: {
+    name: 'My PWA',
+    short_name: 'MyApp',
+    description: 'My Awesome App description',
+    theme_color: '#ffffff',
+    icons: [
+      {
+        src: 'pwa-192x192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        src: 'pwa-512x512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      }
+    ]
+  },
+  devOptions: {
+    enabled: true
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    mkcert(),
+    VitePWA(pwaOptions)
+  ]
 })
+
